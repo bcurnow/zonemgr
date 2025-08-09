b70a98066dabf6a2050ca3b8e6fb096dfe43336b
bcurnow <bcurnow@users.noreply.github.com>
2025-08-08 20:11:59 -0400

    Updated many of the validations to take the identifier and the ResourceRecord. This allows for improved error messaging.

--- plugins/validations.go
+++ plugins/validations.go
@@ -117,41 +117,41 @@ func IsCommentSetInOnePlace(identifier string, rr *schema.ResourceRecord) error 
 
 // Validates that the name provided matches the RFC1035 regex for valid names according to RFC1035
 // and is less then or equal to 255 total characters
-func IsValidRFC1035Name(name string) error {
+func IsValidRFC1035Name(name string, identifier string, rr schema.ResourceRecord) error {
 	if len(name) > 255 {
-		return fmt.Errorf("Must be less than 255 characters: '%s'", name)
+		return fmt.Errorf("%s record invalid, must be less than 255 characters: '%s', identifier=%s", rr.Type, name, identifier)
 	}
 
 	if !dnsNameRegexRFC1035.MatchString(name) {
-		return fmt.Errorf("Does not match regexp '%s': '%s'", dnsNameRegexRFC1035String, name)
+		return fmt.Errorf("%s record invalid, does not match regexp '%s': '%s', identifier=%s", rr.Type, dnsNameRegexRFC1035String, name, identifier)
 	}
 
 	//Split the domain at each part (".") and then run some additional validations
 	parts := strings.Split(name, ".")
 	for _, part := range parts {
 		if strings.HasPrefix(part, "-") || strings.HasSuffix(part, "-") {
-			return fmt.Errorf("Can not start or end with a hyphen (-): '%s'", name)
+			return fmt.Errorf("%s record invalid, Can not start or end with a hyphen (-): '%s', identifier=%s", rr.Type, name, identifier)
 		}
 	}
 	return nil
 }
 
 // Checks if the name provide is either the wildcard ('@') or is a valid name
-func IsValidNameOrWildcard(name string) error {
+func IsValidNameOrWildcard(name string, identifier string, rr schema.ResourceRecord) error {
 	// Check if the name matches the regex or is a wildcard
 	if name == "@" {
 		return nil
 	}
-	return IsValidRFC1035Name(name)
+	return IsValidRFC1035Name(name, identifier, rr)
 }
 
 // Formats and email address according to RFC1035
-func FormatEmail(email string) (string, error) {
+func FormatEmail(email string, identifier string, rr schema.ResourceRecord) (string, error) {
 	if strings.Contains(email, "@") {
 		// Assume this is a standard email address that will be parseable
 		address, err := mail.ParseAddress(email)
 		if err != nil {
-			return "", fmt.Errorf("Invalid email address: %s", email)
+			return "", fmt.Errorf("%s record invalid, invalid email address: %s, identifier=%s", rr.Type, email, identifier)
 		}
 
 		// Get the username portion of the email (<username>@<domain>), keep in mind that valid usernames can continue '@'
@@ -173,18 +173,18 @@ func FormatEmail(email string) (string, error) {
 
 // Most DNS names in a zone file need to be fully qualified domain names, while we can't validate if the entire name itself is valid,
 // we can ensure that it is a valid name and ends with a trailing dot
-func IsFullyQualified(name string) error {
-	if err := IsValidRFC1035Name(name); err != nil {
+func IsFullyQualified(name string, identifier string, rr schema.ResourceRecord) error {
+	if err := IsValidRFC1035Name(name, identifier, rr); err != nil {
 		return err
 	}
 
 	if !hasTrailingDot(name) {
-		return fmt.Errorf("Must end with a trailing dot: '%s'", name)
+		return fmt.Errorf("%s record invalid, must end with a trailing dot: '%s', identifier=%s", rr.Type, name, identifier)
 	}
 
 	// Count the full stops ('.') in the name, there must be at least two (one for the root and one for the domain)
 	if strings.Count(name, ".") < 2 {
-		return fmt.Errorf("Must be fully qualified with at least two dots: '%s'", name)
+		return fmt.Errorf("%s record invalid, must be fully qualified with at least two dots: '%s', identifier=%s", rr.Type, name, identifier)
 	}
 	return nil
 }
